2690a35306827f460fc7411ac2cf14d6a493d15c
Naf9999 <mnafta333@gmail.com>
2026-09-02 12:28:20 +0300

    CORS

--- src/main/java/com/media/photo_video/controller/MediaController.java
+++ src/main/java/com/media/photo_video/controller/MediaController.java
@@ -1,24 +1,29 @@
 package com.media.photo_video.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.media.photo_video.model.Media;
-import com.media.photo_video.services.MediaService;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.media.photo_video.model.Media;
+import com.media.photo_video.services.MediaService;
 
 @RestController
 @RequestMapping("api/media")
-@CrossOrigin
+@CrossOrigin(origins = "https://e-portfolio-backend.onrender.com/")
 public class MediaController {
 
     private final MediaService mediaService;
